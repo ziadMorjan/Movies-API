e19174509f4d1281f30b6d18048cb3d8036e0a13
Ziad Morjan <zmorjan765@gmail.com>
2025-01-12 14:56:31 +0200

    finsh crud on db

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -1,23 +1,95 @@
 const Movie = require("./../models/Movie")
 
 async function getAllMovies(req, res) {
-    res.send("getAllMovies");
+    try {
+        let movies = await Movie.find();
+        res.status(200).json({
+            status: "succsess",
+            length: movies.length,
+            data: {
+                movies
+            }
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error.message,
+            data: null
+        });
+    }
 }
 
 async function createMovie(req, res) {
-    res.send("createMovie");
+    try {
+        let movie = await Movie.create(req.body);
+        res.status(201).json({
+            status: "success",
+            data: {
+                movie
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message,
+            data: null
+        });
+    }
 }
 
-function getSingleMovie(req, res) {
-    res.send("getSingleMovie");
+async function getSingleMovie(req, res) {
+    try {
+        let movie = await Movie.findById(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: {
+                movie
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message,
+            data: null
+        });
+    }
 }
 
-function updateMovie(req, res) {
-    res.send("updateMovie");
+async function updateMovie(req, res) {
+    try {
+        let movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: "success",
+            data: {
+                movie
+            }
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error.message,
+            data: null
+        });
+    }
 }
 
-function deleteMovie(req, res) {
-    res.send("updateMovie");
+async function deleteMovie(req, res) {
+    try {
+        await Movie.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: "success",
+            data: null
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: error.message,
+            data: null
+        });
+    }
 }
 
 module.exports = {
