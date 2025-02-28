022a218e4de29b05b2e37fca08d519837fb258d5
Ziad Morjan <zmorjan765@gmail.com>
2025-02-28 02:08:00 +0200

    add dev/prod error

--- controllers/ErrorController.js
+++ controllers/ErrorController.js
@@ -1,15 +1,40 @@
 function globalErrorHandler(error, req, res, next) {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || "error";
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+    if (process.env.NODE_ENV == "development") {
+        devError(error, res);
+    }
+    else if (process.env.NODE_ENV == "production") {
+        prodError(error, res);
+    }
 }
 
 function asyncErrorHandler(asyncFunc) {
     return (req, res, next) => {
         asyncFunc(req, res, next).catch(err => next(err));
+    }
+}
+
+function devError(error, res) {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        stack: error.stack
+    });
+}
+
+function prodError(error, res) {
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message
+
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "There is some thing wrong! pleas try again later."
+        });
     }
 }
 
--- utils/CustomError.js
+++ utils/CustomError.js
@@ -4,5 +4,6 @@ module.exports = class CustomError extends Error {
         this.statusCode = statusCode;
         this.status = statusCode > 400 && statusCode < 500 ? "fail" : "error";
         this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
     }
 }
