296b51e154854bd75ffe2d6deda495feadf2a009
Ziad Morjan <zmorjan765@gmail.com>
2025-03-09 11:29:23 +0200

    add Unhandled Rejection & Uncaught Exception error Handler

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const dotenv = require("dotenv");
-const app = require("./app");
 const db = require("./config/db");
 
+// Uncaught Exception HAndler
+process.on("uncaughtException", (error) => {
+    console.log(`${error.name}: ${error.message}`);
+    console.log("Uncaught Exception occurred! shutting down...");
+    process.exit(1);
+});
+
+const app = require("./app");
 dotenv.config({ path: "./config.env" });
 
 // start the server
 let port = process.env.PORT || 8000;
 let hostName = process.env.HOST_NAME || "localhost";
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
     console.log(`Mode => ${process.env.NODE_ENV}`);
     console.log(`Server started on => http://${hostName}:${port}`);
 });
 
 // connect to db
 db.connect(process.env.CON_STR);
+
+// Unhandled Rejection HAndler
+process.on("unhandledRejection", (error) => {
+    console.log(`${error.name}: ${error.message}`);
+    console.log("Unhandled Rejection occurred! shutting down...");
+    server.close(() => {
+        process.exit(1);
+    });
+});
